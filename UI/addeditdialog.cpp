6d9e2127c3b50e9165b34859fa832abf9bda4844
GlalexHSE <alseglukhov@edu.hse.ru>
2024-06-11 03:18:53 +0300

    CompletedDialogandMain

---
+++ UI/addeditdialog.cpp
@@ -0,0 +1,76 @@
+#include "addeditdialog.h"
+#include "ui_addeditdialog.h"
+
+EntryDialog::EntryDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::EntryDialog)
+{
+    ui->setupUi(this);
+
+
+}
+
+EntryDialog::~EntryDialog()
+{
+    delete ui;
+}
+
+QString EntryDialog::getTitle() const
+{
+    return ui->titleInput->text();
+}
+
+void EntryDialog::setTitle(const QString &title)
+{
+    ui->titleInput->setText(title);
+}
+
+QString EntryDialog::getUsername() const
+{
+    return ui->usernameInput->text();
+}
+
+void EntryDialog::setUsername(const QString &username)
+{
+    ui->usernameInput->setText(username);
+}
+
+QString EntryDialog::getPassword() const
+{
+    return ui->passwordInput->text();
+}
+
+void EntryDialog::setPassword(const QString &password)
+{
+    ui->passwordInput->setText(password);
+}
+
+QString EntryDialog::getUrl() const
+{
+    return ui->urlInput->text();
+}
+
+void EntryDialog::setUrl(const QString &url)
+{
+    ui->urlInput->setText(url);
+}
+
+QString EntryDialog::getTag() const
+{
+    return ui->tagInput->text();
+}
+
+void EntryDialog::setTag(const QString &tag)
+{
+    ui->tagInput->setText(tag);
+}
+
+void EntryDialog::on_okButton_clicked()
+{
+    accept();
+}
+
+void EntryDialog::on_cancelButton_clicked()
+{
+    reject();
+}
